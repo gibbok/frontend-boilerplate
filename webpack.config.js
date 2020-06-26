6f438bd5a70f2003b0a6390a97cd6c12805ea7a5
GibboK <gibbok.coding@gmail.com>
2020-06-26 17:07:22 +0200

    fix path

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
+    publicPath: './',
   },
   // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
   resolve: {
