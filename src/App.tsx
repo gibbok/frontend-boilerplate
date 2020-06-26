01533531c23f0af7453962e1b80b13a74f9a4bc8
GibboK <gibbok.coding@gmail.com>
2020-06-26 09:32:00 +0200

    add support images

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import GlobalStyle from './utils/GlobalStyle';
 import styled from 'styled-components';
+import logo from './assets/logo.svg';
 
 export function App(): JSX.Element {
   return (
     <>
       <GlobalStyle />
       <AppS>Hello world!😎</AppS>
+      <img src={logo} alt="an image" width="100" height="100"></img>
     </>
   );
 }
