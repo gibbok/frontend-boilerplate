04201a07127a0f083d9752258a8e623b517b1b10
GibboK <gibbok.coding@gmail.com>
2020-06-26 16:56:12 +0200

    fix issue with hardcoded 8080

--- cypress/integration/hello.ts
+++ cypress/integration/hello.ts
@@ -1,6 +1,6 @@
 describe('hello', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080');
+    cy.visit('/');
   });
 
   it('should render Hello', () => {
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,6 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
-    // publicPath: '/',
   },
   // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
   resolve: {
