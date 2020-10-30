ccaec9147b3de677e8b90ad23f9573f7bbb9ea5d
GibboK <gibbok.coding@gmail.com>
2020-10-30 21:27:17 +0100

    simplify test

--- cypress/integration/get-started.ts
+++ cypress/integration/get-started.ts
@@ -1,88 +1,55 @@
 import { mkRandomEmail, mkRandomString } from '../utils';
 
 describe('actyx.com/get-started/', () => {
-  const URL_G_START = 'https://www.actyx.com/get-started/';
-  // const URL_G_END_THANKS = 'https://www.actyx.com/thank-you'; // TODO read above
-  const URL_G_END_DEVELOPER = 'https://developer.actyx.com/docs/home/home';
+  const URL_GETSTARTED = 'https://www.actyx.com/get-started/';
+  const URL_DEVELOPER = 'https://developer.actyx.com/docs/home/home';
 
   const TEST_EMAIL = mkRandomEmail();
   const TEST_DETAILS = `test - ${mkRandomString()} - I am a robot adding details`;
 
-  const checkThankYou = () => {
-    // assert submit was successful
-    // TODO uncomment after #5296 is completed
-    // cy.contains('Thank you!').should('exist');
-    // cy.location().should((loc) => {
-    //   expect(loc.href).to.eq(URL_G_END_THANKS);
-    // });
-  };
-
-  const submitForm = () => {
-    cy.get('button[type="submit"]').should('exist');
-    // TODO uncomment after #5296 is completed
-    // const btnSumbit = cy.get('button[type="submit"]').should('exist');
-    // btnSumbit.click();
-  };
-
   it('should schedule a call for a factory manager', () => {
-    cy.visit(URL_G_START);
+    cy.visit(URL_GETSTARTED);
 
-    // find button on page
     const btnScheduleCall = cy.contains('Schedule a call');
     btnScheduleCall.should('exist');
 
-    // click button and open dialog
     btnScheduleCall.click();
     const inputEmail = cy.get('input[name="email"]').should('exist');
 
-    // enter email
     inputEmail.click();
     inputEmail.type(TEST_EMAIL);
 
-    // enter details
-    const inputDetails = cy.get('textarea[name="additionalDetails"]').should('exist');
+    const inputDetails = cy
+      .get('textarea[name="additionalDetails"]')
+      .should('exist');
     inputDetails.type(TEST_DETAILS);
-
-    submitForm();
-
-    checkThankYou();
   });
 
   it('should schedule a demo for an it project manager', () => {
-    cy.visit(URL_G_START);
+    cy.visit(URL_GETSTARTED);
 
-    // find button on page
     const btnGetDemo = cy.contains('Get a demo');
     btnGetDemo.should('exist');
 
-    // click button and open dialog
     btnGetDemo.click();
     const inputEmail = cy.get('input[name="email"]').should('exist');
 
-    // enter email
     inputEmail.click();
     inputEmail.type(TEST_EMAIL);
 
-    // enter details
-    const inputDetails = cy.get('textarea[name="additionalDetails"]').should('exist');
+    const inputDetails = cy
+      .get('textarea[name="additionalDetails"]')
+      .should('exist');
     inputDetails.type(TEST_DETAILS);
-
-    submitForm();
-
-    checkThankYou();
   });
 
-  it('should redirect a developer to developer site', () => {
-    cy.visit(URL_G_START);
+  it.only('should redirect a developer to developer site', () => {
+    cy.visit(URL_GETSTARTED);
 
-    // find button on page
     const btnGetStart = cy.get('button').contains('Get started');
     btnGetStart.should('exist');
 
-    // click button and redirect user
     btnGetStart.click();
-    cy.location().should((loc) => {
-      expect(loc.href).to.eq(URL_G_END_DEVELOPER);
-    });
+    cy.url().should('equal', URL_DEVELOPER);
   });
 });
